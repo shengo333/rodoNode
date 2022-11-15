b898f1c53eecc31b6a4069a4434cea0672e8adad
Beka Shengelia <bekashengelia333@gmail.com>
2022-11-15 10:19:06 +0400

    some more setup

---
+++ controllers/todoController.js
@@ -0,0 +1,11 @@
+module.exports = function(app){
+    app.get('/todo', function(req, res){
+        
+    });
+    app.post('/todo', function(req, res){
+
+    });
+    app.delete('/todo', function(req,res){
+
+    });
+};
