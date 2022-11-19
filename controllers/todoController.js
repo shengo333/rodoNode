22cfcfd3ef48fc05d1a5d36ae5dbe6f3adcfb28c
Beka Shengelia <bekashengelia333@gmail.com>
2022-11-19 08:11:25 +0400

    first data saved on mongo

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,7 +1,29 @@
 let bodyParser = require('body-parser');
+let mongoose = require('mongoose');
+
+
+
+//coect to database
+
+
+mongoose.connect('mongodb+srv://shengo333:shengo333@todo.wdplkzs.mongodb.net/?retryWrites=true&w=majority')
+
+
+//create a schema - this is like blueprint
+
+let todoSchema = new mongoose.Schema({
+    item: String
+})
+
+let Todo = mongoose.model('Todo', todoSchema);
+
+let itemOne = Todo({item: 'buy flowers'}).save(function(err){
+    if(err) throw err;
+    console.log('item saved')
+})
 
 let data = [{item: 'get milk'}, {item: 'walk dog'}, {item: 'kick some coding ass'}];
-var urlencodedParser = bodyParser.urlencoded({extended: false})
+let urlencodedParser = bodyParser.urlencoded({extended: false})
 
 
 module.exports = function(app){
